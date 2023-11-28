af7030c41b4d7a692219f9533c033b70fedbe213
김도현 <swputer@gmail.com>
2023-11-29 06:57:58 +0900

    build: 프론트엔드 환경 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,32 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RecoilRoot } from 'recoil';
+
+import GlobalStyle from '@styles/global';
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <></>,
+  },
+]);
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <GlobalStyle />
+          <RouterProvider router={router} />
+        </RecoilRoot>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default App;
