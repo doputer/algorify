95a8c75c2d4c7d76439da72ffb46f2dba7f74dcb
김도현 <swputer@gmail.com>
2023-12-11 08:00:02 +0900

    build: 프론트엔드 환경 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,8 @@
-import svgr from '@svgr/rollup';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
 });
