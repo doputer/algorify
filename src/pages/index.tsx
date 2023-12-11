b9d9ddad9361524eb9026b04e155857942c30e1b
김도현 <swputer@gmail.com>
2023-12-12 06:19:44 +0900

    feat: 메인 레이아웃 추가 및 GraphQL 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,33 @@
-import type { HeadFC, PageProps } from 'gatsby';
+import { graphql, type HeadFC, type PageProps } from 'gatsby';
 
 import Header from '@/components/header';
 import Main from '@/components/main';
 import Nav from '@/components/nav';
 
-const IndexPage: React.FC<PageProps> = () => {
+function IndexPage({ data }: PageProps<Queries.AlgorithmQuery>) {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <div className="flex flex-auto">
         <Nav />
-        <Main />
+        <Main content={data.mdx} />
       </div>
     </div>
   );
-};
+}
 
 export default IndexPage;
 
 export const Head: HeadFC = () => <title>Visual Algorithm</title>;
+
+export const query = graphql`
+  query Algorithm($slug: String) {
+    mdx(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        type
+        title
+      }
+      body
+    }
+  }
+`;
