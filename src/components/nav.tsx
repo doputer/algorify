c09c24382b6ff00f866f297a78e5bb2d58d8f50f
김도현 <swputer@gmail.com>
2023-12-12 07:57:30 +0900

    feat: 네비게이션 GraphQL 적용

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,35 +1,61 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+interface Node {
+  frontmatter: {
+    type: string;
+    title: string;
+  };
+}
+
 function Nav() {
-  const groups = [
-    {
-      name: '브루트 포스 (Brute Force)',
-      groupItems: [
-        { name: '버블 정렬 (Bubble Sort)', to: 'bubble-sort' },
-        { name: '삽입 정렬 (Insertion Sort)', to: 'insertion-sort' },
-        { name: '선택 정렬 (Selection Sort)', to: 'selection-sort' },
-      ],
-    },
-  ];
+  const { allMdx } = useStaticQuery(graphql`
+    query {
+      allMdx {
+        nodes {
+          frontmatter {
+            type
+            title
+          }
+        }
+      }
+    }
+  `);
+
+  const items: { [key: string]: string[] } = {};
+
+  allMdx.nodes.forEach(({ frontmatter }: Node) => {
+    if (frontmatter.type in items) {
+      items[frontmatter.type].push(frontmatter.title);
+    } else {
+      items[frontmatter.type] = [frontmatter.title];
+    }
+  });
+
+  const isCurrentPath = (path: string) => window.location.pathname === `/${path.toLowerCase()}/`;
 
   return (
     <aside className="border-r-1 sticky left-0 top-14 flex h-[calc(100vh-3.5rem)] min-w-[300px] flex-col items-center overflow-y-scroll border-r border-solid border-gray-200 px-8 pt-6">
-      <div className="flex h-full w-full flex-col">
-        {groups.map((group, groupIndex) => {
+      <div className="flex h-full w-full flex-col gap-4">
+        {Object.entries(items).map(([groupName, groupItems]) => {
           return (
-            <>
-              <div key={groupIndex} className="py-3 font-bold">
-                {group.name}
-              </div>
+            <div key={groupName}>
+              <div className="pb-2 font-bold">{groupName.split('-').join(' ')}</div>
               <ul>
-                {group.groupItems.map((groupItem, index) => (
-                  <li
-                    key={index}
-                    className="border-l border-solid border-gray-300 px-2 py-2 font-medium hover:border-gray-500 hover:font-semibold"
-                  >
-                    {/* <Link to={'/' + groupItem.to}>{groupItem.name}</Link> */}
-                  </li>
+                {groupItems.map((groupItem) => (
+                  <a key={groupItem} href={'/' + groupItem.toLowerCase()}>
+                    <li
+                      className={`border-l border-solid px-2 py-1 font-medium hover:border-gray-500 ${
+                        isCurrentPath(groupItem)
+                          ? 'border-gray-500'
+                          : 'border-gray-300 text-muted-foreground'
+                      }`}
+                    >
+                      {groupItem.split('-').join(' ')}
+                    </li>
+                  </a>
                 ))}
               </ul>
-            </>
+            </div>
           );
         })}
       </div>
