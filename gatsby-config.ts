1d589174391db500ed639a341266d49235d75527
김도현 <swputer@gmail.com>
2023-12-13 20:08:18 +0900

    build: 프론트엔드 환경 설정

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,24 +1,23 @@
-import path from 'path';
-
 import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `visual-algorithm`,
+    title: `Algorify`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   graphqlTypegen: true,
+  trailingSlash: 'never',
   plugins: [
+    'gatsby-plugin-postcss',
     'gatsby-plugin-mdx',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'posts',
-        path: path.resolve(__dirname, 'src/posts'),
+        path: `${__dirname}/src/posts`,
       },
+      __key: 'posts',
     },
-    'gatsby-plugin-typescript',
-    'gatsby-plugin-postcss',
   ],
 };
 
--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,16 +1,41 @@
 import path from 'path';
 
+import { GatsbyNode } from 'gatsby';
 import { createFilePath } from 'gatsby-source-filesystem';
 
 export const onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
       alias: {
-        '@/components': path.resolve(__dirname, 'src/components'),
-        '@/lib/utils': path.resolve(__dirname, 'src/lib/utils'),
+        '@': path.resolve(__dirname, 'src'),
       },
     },
   });
+};
+
+export const createSchemaCustomization: GatsbyNode['createSchemaCustomization'] = ({ actions }) => {
+  actions.createTypes(`
+    type PagesQuery {
+      allMdx: AllMdx!
+    }
+
+    type PageQuery {
+      mdx: Nodes!
+    }
+
+    type AllMdx {
+      nodes: [Nodes!]!
+    }
+
+    type Nodes {
+      frontmatter: Frontmatter!
+    }
+
+    type Frontmatter {
+      type: String!
+      title: String!
+    }
+  `);
 };
 
 export const onCreateNode = ({ node, actions, getNode }) => {
@@ -41,15 +66,20 @@ export const createPages = async ({ graphql, actions }) => {
             type
             title
           }
+          internal {
+            contentFilePath
+          }
         }
       }
     }
   `);
 
+  const postTemplate = path.resolve(__dirname, `src/pages/post.tsx`);
+
   result.data.allMdx.nodes.forEach((node) => {
     createPage({
       path: node.fields.slug,
-      component: path.resolve(__dirname, `src/pages/index.tsx`),
+      component: `${postTemplate}?__contentFilePath=${node.internal.contentFilePath}`,
       context: {
         slug: node.fields.slug,
       },
