00e58af99902d78e0e764328ed1b3b80e329d5e7
김도현 <swputer@gmail.com>
2023-12-13 20:09:14 +0900

    feat: 포스트 페이지 추가

---
+++ src/pages/post.tsx
@@ -0,0 +1,36 @@
+import { ChevronRightIcon } from '@radix-ui/react-icons';
+import { graphql } from 'gatsby';
+
+interface PostPageProps {
+  data: Queries.PageQuery;
+  children: JSX.Element;
+}
+
+function PostPage({ data, children }: PostPageProps) {
+  const { type, title } = data.mdx.frontmatter;
+
+  return (
+    <>
+      <div className="mb-2 flex items-center text-lg">
+        <div className="text-muted-foreground">{type}</div>
+        <ChevronRightIcon className="mb-0.5" />
+        <div className="font-semibold">{title}</div>
+      </div>
+      <h1 className="mb-12 text-4xl font-extrabold tracking-tight">{title}</h1>
+      <div>{children}</div>
+    </>
+  );
+}
+
+export default PostPage;
+
+export const query = graphql`
+  query Page($slug: String) {
+    mdx(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        type
+        title
+      }
+    }
+  }
+`;
