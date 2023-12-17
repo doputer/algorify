1e4fcf0884d1d309a3177c550e29040de4fe152d
김도현 <swputer@gmail.com>
2023-12-18 01:46:21 +0900

    feat: Quick Sort 추가

--- src/components/visualizer/useView.tsx
+++ src/components/visualizer/useView.tsx
@@ -4,7 +4,8 @@ import bubble from './algorithm/bubble-sort';
 import cocktail from './algorithm/cocktail-sort';
 import comb from './algorithm/comb-sort';
 import insertion from './algorithm/insertion-sort';
-import selection from './algorithm/selectioin-sort';
+import quick from './algorithm/quick-sort';
+import selection from './algorithm/selection-sort';
 import Controller from './controller';
 import Viewer from './viewer';
 
@@ -14,6 +15,7 @@ const algorithm = {
   INSERTION: 'insertion',
   COCKTAIL: 'cocktail',
   COMB: 'comb',
+  QUICK: 'quick',
 };
 
 const generator: {
@@ -24,6 +26,7 @@ const generator: {
   [algorithm.INSERTION]: insertion,
   [algorithm.COCKTAIL]: cocktail,
   [algorithm.COMB]: comb,
+  [algorithm.QUICK]: quick,
 };
 
 const useView = (algorithm: string, initValues: number[]) => {
